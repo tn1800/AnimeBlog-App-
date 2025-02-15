fe20e0e0442364e14fefea974cae87ce32c46d9d
TristanNgo <obeselegend12@gmail.com>
2025-02-14 20:02:20 -0800

    implemented base layout of anime posts
    for each anime blog post, the user will an image of the anime, its name, author, date of creation, as well as their own personal rating out of 10.

---
+++ client/app/src/Post.js
@@ -0,0 +1,21 @@
+export default function Post() {
+    return(
+        <div className="post">
+        <div className = "image">
+        <img src="https://thelimitlessimagination.wordpress.com/wp-content/uploads/2021/05/steins-gate-cover-1.jpg" alt = "No Image Right Now" />
+        </div>
+       <div className="texts">
+        <h2> Steins Gate </h2>
+        <p className="info">
+          <a className="author"> Yomi Sarachi </a>
+          <date> 09/14/2011 </date>
+          <a className ="rating"> 9.5/10 </a>
+        </p>
+        <p className = "summary"> Eccentric scientist Rintarou Okabe's clumsy but well intentioned friend Mayuri and his roommate Itaru Hashida, Okabe founds the Future Gadget Laboratory in the hopes of creating technological innovations that baffle the human psyche. Despite claims of grandeur, the only notable "gadget" the trio have created is a microwave that has the mystifying power to turn bananas into green goo.
+However, when Okabe attends a conference on time travel, he experiences a series of strange events that lead him to believe that there is more to the "Phone Microwave" gadget than meets the eye. Apparently able to send text messages into the past using the microwave, Okabe dabbles further with the "time machine," attracting the ire and attention of the mysterious organization SERN.
+Due to the novel discovery, Okabe and his friends find themselves in an ever-present danger. As he works to mitigate the damage his invention has caused to the timeline, Okabe fights a battle to not only save his loved ones but also to preserve his degrading sanity.  </p>
+        </div>
+      </div>
+    )
+    
+}
