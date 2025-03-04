ff7c9af3c543e7bc2d20ea64042d4539134a89ff
TristanNgo <obeselegend12@gmail.com>
2025-03-03 21:36:24 -0800

    change the dark mode button
    make the dark button smaller, and into the moon.

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -21,22 +21,24 @@ function App() {
   }
   return (
     <div className={darkMode ? "dark-mode" : ""}>
-      <button onClick={toggleDarkMode}>Dark Mode</button>
-    <UserContextProvider> 
-    <Routes> 
-      <Route path="/" element={<Layout />} > 
-      <Route index element ={<IndexPage /> } /> 
-     <Route path= "/login" element= {<LoginPage />} />
-     <Route path="/register" element={<RegisterPage />} />
-     <Route path="/create" element={<CreatePost />} />
-     <Route path="/post/:id" element={<PostPage />} />
-     <Route path="/edit/:id" element={<EditPost />} />
-   </Route>
-    </Routes>
-    
-    </UserContextProvider>
+      <div className="dark-mode-toggle">
+        <button onClick={toggleDarkMode}>🌙</button> 
+      </div>
+
+      <UserContextProvider> 
+        <Routes> 
+          <Route path="/" element={<Layout />} > 
+            <Route index element={<IndexPage />} /> 
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/post/:id" element={<PostPage />} />
+            <Route path="/edit/:id" element={<EditPost />} />
+          </Route>
+        </Routes>
+      </UserContextProvider>
     </div>
-  ); 
+  );
 }
 
 export default App;
